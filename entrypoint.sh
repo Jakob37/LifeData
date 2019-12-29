fafe5dd3daa68d242d44ab1683fe4afd98a7b337
Daniel Alm Grundström <github@grundstrom.email>
2019-12-29 22:44:45 +0100

    Adds execution of graph autogeneration script in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+echo "GRAPH_DIR=$GRAPH_DIR"
 echo "DATA_DIR=$DATA_DIR"
+
+(
+	/usr/local/bin/run_when_changed.sh \
+		watch="$DATA_DIR" \
+		files="sleep.csv" \
+		script="/usr/local/bin/on_sleep.csv_changed.sh"
+) &
+
+exec sleep infinity
